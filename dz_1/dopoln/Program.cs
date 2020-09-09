85f08baac6e4940afd12ad75b00c78ed533d0136
Cavid <cavidmammad@mail.ru>
2020-09-09 12:53:58 +0300

    My homeworks

---
+++ dz_1/dopoln/Program.cs
@@ -0,0 +1,107 @@
+﻿
+
+using OpenTK;
+using System;
+
+class Student
+{
+    public string Surname;
+    public string Name;
+    public int Course;
+    private int num;
+    public int Book_num
+    {
+        set { num = value;}
+        get { return num; }
+    }
+    public Student() { }
+    public Student(string surname,string name,int course,int book_num)
+    {
+        Surname = surname; Name = name; Course = course; Book_num = book_num;
+    }
+}
+
+class Aspirant:Student
+{
+    public Aspirant(string surname, string name, int course, int book_num)
+        : base(surname, name, course, book_num)
+    {
+
+    }
+
+    public void  Print()
+    {
+        Console.WriteLine($"\nAspirant {Name} {Surname} study in {Course} course by grade book number {Book_num}");
+    }
+}
+
+class Call
+{
+    static int student_num = 1;
+    static int aspirant_num = 1;
+    static void Define(int x=0)
+    {
+        Student student = new Student();
+        bool check = true;
+
+        while (check == true)
+        {
+            Console.Write("Введите имя студента - ");
+            student.Name = Console.ReadLine();
+            Console.Write("Введите фамилию студента - ");
+            student.Surname = Console.ReadLine();
+            foreach (char a in student.Name + student.Surname)
+            {
+                if (a >= '0' && a <= '9')
+                {
+                    Console.WriteLine("Попробуйте ввести  еще раз!\n");
+                    check = true;
+                    break;
+                }
+                else
+                {
+                    check = false;
+                }
+            }
+        }
+        
+
+        Console.Write("Выберите курс обучения - ");
+        student.Course = int.Parse(Console.ReadLine());
+        Console.Write("Выберите номер зачетной книги - ");
+        student.Book_num = int.Parse(Console.ReadLine());
+        if (student.Course > 5 || student.Course < 1 || student.Book_num < 1) { Console.WriteLine("Введены невалидные данные!"); return; }
+        else
+        {
+            Aspirant aspirant = new Aspirant(student.Name, student.Surname, student.Course, student.Book_num);
+            aspirant.Print();
+            Recurs();
+            
+        }
+    }
+
+    static void Recurs()
+    {
+        Console.Write("Если хотите добавить добавить студентов нажмите - 1,если нет то - 2 :  ");
+        int given = int.Parse(Console.ReadLine());
+        if (given == 1)
+        {
+              student_num ++; 
+              aspirant_num ++;
+              Define();
+
+        }
+        else if(given==2)
+        {
+            Console.WriteLine($"Количество студентов - {student_num},количество аспирантов -{aspirant_num}");
+            return;
+        }
+        
+    }
+
+    static void Main()
+    {
+        Console.WriteLine($"Привет!");
+        Define();
+    }
+}
---
+++ dz_2/dz_2/Program.cs
@@ -0,0 +1,153 @@
+﻿
+using System;
+
+class Account
+{
+    public double Total;
+    public int Account_num;
+    public int Date;
+
+    public Account() { }
+    public Account(double total,int account_num,int date)
+    {
+        Total = total; Account_num = account_num; Date = date;
+    }
+    public void Amount()
+    {
+        Console.WriteLine($"Сумма в вашем счету - {Total}");
+    }
+    public void Date_Screen()
+    {
+        Console.WriteLine($"Дата открытия счета - {Date}");
+    }
+}
+
+class Individual:Account
+{
+    public string Account_type;
+    public Individual(double total, int account_num, int date,string account_type)
+        :base(total,account_num,date)
+    {
+        Account_type = account_type;
+    }
+
+    public void Withdrawing()
+    {
+        Console.Write("\nВведите сумму которые хотите снять : ");
+        int money = int.Parse(Console.ReadLine());
+        if (money < 0 || money > Total) { Console.Write("Введены невалидные данные!"); return; }
+        Console.WriteLine($"Снято - {money} Azn,в счету {Total - money} Azn");
+    }
+    public void Percent()
+    {
+        Console.WriteLine("Вычисление для физических лиц!");
+        Console.Write("Выберите Процент займа : ");
+        int percent = int.Parse(Console.ReadLine());
+        if (percent < 0 || percent > 100) { Console.Write("Введены неверные данные!");  return; }
+
+        Console.Write("Выберите период года займа : ");
+        int period = int.Parse(Console.ReadLine());
+        if (period < 0) { Console.Write("Введены неверные данные!"); return; }
+
+        Console.Write("Для простого начисления процента нажимаем - 1,для сложного 2 : ");
+        int given = int.Parse(Console.ReadLine());
+        double result =Total;
+
+        if (given == 1)
+        {
+            for(int i = 1; i <= period; i++)
+            {
+                result += Total*percent/100;
+            }
+            Console.WriteLine("Результат суммы - " + result);
+        }
+
+        else if (given == 2)
+        {
+            for (int i = 1; i <= period; i++)
+            {
+                result += Total * percent / 100;
+                Total = result;
+            }
+            Console.WriteLine("Результат суммы - " + result);
+        }
+        else { Console.Write("Введены неверные данные!"); return; }
+    }
+
+}
+
+class Entity:Account
+{
+    public Entity(double total, int account_num, int date)
+        : base(total, account_num, date) { }
+
+    public void Percent()
+    {
+        Console.WriteLine("\nВычисление для юридических лиц!");
+        Console.Write("Выберите Процент займа : ");
+        int percent = int.Parse(Console.ReadLine());
+        if (percent < 0 || percent > 100) { Console.Write("Введены неверные данные!"); return; }
+
+        Console.Write("Выберите период займа : ");
+        int period = int.Parse(Console.ReadLine());
+        if (period < 0) { Console.Write("Введены неверные данные!"); return; }
+
+        Console.Write("Для простого начисления процента нажимаем - 1,для сложного 2 : ");
+        int given = int.Parse(Console.ReadLine());
+        double result = Total;
+
+        if (given == 1)
+        {
+            for (int i = 1; i <= period; i++)
+            {
+                result += Total * percent / 100;
+                
+            }
+            Console.WriteLine("Результат суммы -" + result);
+        }
+
+        else if (given == 2)
+        {
+            for (int i = 1; i <= period; i++)
+            {
+                result += Total * percent / 100;
+                Total = result;
+            }
+            Console.WriteLine("Результат суммы -" + result);
+        }
+        else { Console.Write("Введены неверные данные!"); return; }
+    }
+}
+
+class Do
+{
+    static void Main()
+    {
+        Account person = new Account();
+
+        Console.Write("Выбериту сумму на счету:");
+        person.Total = double.Parse(Console.ReadLine());
+        if (person.Total < 0) { Console.Write("Введены невалидные данные!");return; }
+
+        Console.Write("Выбериту текущий год открытия  счета:");
+        person.Date = int.Parse(Console.ReadLine());
+        if (person.Date != 2020) { Console.Write("Введены невалидные данные!"); return; }
+
+        Console.Write("Выбериту номер счета:");
+        person.Account_num = int.Parse(Console.ReadLine());
+        if (person.Total <0) { Console.Write("Введены невалидные данные!"); return; }
+
+        person.Amount();
+        person.Date_Screen();
+
+        Individual individual = new Individual(person.Total,person.Date,person.Account_num,"Current");
+        individual.Withdrawing();
+        individual.Percent();
+        
+
+        Entity entity = new Entity(person.Total, person.Date, person.Account_num);
+        entity.Percent();
+
+        Console.ReadKey();
+    }
+}
